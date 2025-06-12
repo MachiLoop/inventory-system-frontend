10b8f033f24ff5add52bece3b7a859dbe92cfb36
machiloop <hamidusodiq2@gmail.com>
2025-06-12 20:28:51 +0100

    feat(ui): styled categories card component

--- components/itemCard.jsx
+++ components/itemCard.jsx
@@ -7,14 +7,14 @@ import React from "react";
 
 const ItemCard = ({ item, handleEditCategory }) => {
   return (
-    <View className="flex-row justify-between items-center border-neutral-500 mb-4 p-2 gap-4 ">
-      <View className="flex-row items-center gap-2 flex-1">
+    <View className="flex-row justify-between items-center bg-gray-50 mb-4 p-2 gap-4 rounded-lg mx-2">
+      <View className=" gap-2 flex-1">
         <Image source={{ uri: item.imageUrl }} style={styles.logo} />
-        <Text className="capitalize ">{item.name}</Text>
+        <Text className="capitalize text-base font-psemibold">{item.name}</Text>
       </View>
       <View className="flex-row gap-4 items-center">
         <TouchableOpacity className="bg-bgBlue px-4 rounded-full">
-          <Text className="text-xl text-contentBlue">view</Text>
+          <Text className="text-lg text-contentBlue">view</Text>
         </TouchableOpacity>
 
         <TouchableOpacity onPress={() => handleEditCategory(item)}>
